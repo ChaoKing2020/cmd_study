0e6b448f49186b8771586a4f81ae9017c8ff3ecd
wangchao <183799815@qq.com>
2022-08-20 10:15:08 +0800

    'init'

---
+++ cmd.bat
@@ -0,0 +1,7 @@
+d:
+cd study\Ñ§Ï°±Ê¼Ç
+git add .
+git commit -m 'init'
+git pull git@github.com:ChaoKing2020/Linux_study.git main
+git push git@github.com:ChaoKing2020/Linux_study.git master:main
+pause
