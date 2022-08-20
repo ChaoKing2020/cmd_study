dc459c4b1c14fe8d37ee2b03eee928b3c8a54716
wangchao <183799815@qq.com>
2022-08-20 10:17:34 +0800

    'init'

--- cmd.bat
+++ cmd.bat
@@ -1,5 +1,5 @@
 git add .
 git commit -m 'init'
-git pull git@github.com:ChaoKing2020/Linux_study.git main
-git push git@github.com:ChaoKing2020/Linux_study.git master:main
+git pull git@github.com:ChaoKing2020/cmd_study.git main
+git push git@github.com:ChaoKing2020/cmd_study.git master:main
 pause
